a5b9734bdcd38a7b669b2d36191e883d6ff81847
Alspirid <Aleksey.Spiridonov@gmail.com>
2018-02-13 11:18:15 -0800

    Add sounds and Play/Pause

--- scripts/canvas.js
+++ scripts/canvas.js
@@ -1,25 +1,37 @@
 
+let isPaused = true;
+let isStart = true;
 
-const player = {
+const packman = {
   x: 50,
   y: 100,
   pacmouth:320,
   pacdir: 0,
   pSize: 32,
-  speed: 5,
+  speed: 15,
 };
 
 const enemy = {
   x: 150,
   y: 200,
   speed: 5,
-  moving: 0,
+  movingTime: 0,
   dirX: 0,
   dirY: 0,
+  flash: 0,
+  ghostEat: false,
 };
 
+const powerball = {
+  x: 10,
+  y: 10,
+  visible: false,
+  pcountdown: 0,
+  ghostNum: 0,
+  ghostEat: false,
+};
 
-let score = 0, gscore = 0, ghost = false;
+let score = 0, gscore = 0, ghost = false, countblink = 10;
 
 
 const canvas = document.createElement("canvas");
@@ -32,62 +44,96 @@ mainImage.src = "./assets/pac.png";
 mainImage.onload = checkReady;
 
 document.getElementById('root').appendChild(canvas);
+const soundChomp = new Audio('./assets/pacman_chomp.wav');
+const soundEatFruit = new Audio('./assets/pacman_eatfruit.wav');
+const soundEatGhost = new Audio('./assets/pacman_eatghost.wav');
+const soundDeath = new Audio('./assets/pacman_death.wav');
+const soundBeginning = new Audio('./assets/pacman_beginning.wav');
 
 const keyClick = {};
 
 document.addEventListener('keydown', (e)=>{
   keyClick[e.keyCode]=true;
   move(keyClick);
+  togglePause(e.keyCode);
 },false);
 
 document.addEventListener('keyup', (e)=>{
   delete keyClick[e.keyCode];
 },false);
 
+
+const togglePause = (key) => {
+  if (key === 32) {
+    isPaused = ! isPaused;
+    playGame();
+  }
+};
+
+// const playSound = (key) => {
+//   switch (key) {
+//     case 37,38,39,40:
+//       soundChomp.play();
+//       break;
+//   }
+// };
+
 const move = (keyObject) => {
 
   if (37 in keyObject) {
-    player.x -= player.speed; player.pacdir = 64;
+    packman.x -= packman.speed; packman.pacdir = 64;
+    // soundChomp.play();
   }
   if (38 in keyObject) {
-    player.y -= player.speed; player.pacdir = 96;
+    packman.y -= packman.speed; packman.pacdir = 96;
+    // soundChomp.play();
   }
   if (39 in keyObject) {
-    player.x += player.speed; player.pacdir = 0;
+    packman.x += packman.speed; packman.pacdir = 0;
+    // soundChomp.play();
   }
   if (40 in keyObject) {
-    player.y += player.speed; player.pacdir = 32;
+    packman.y += packman.speed; packman.pacdir = 32;
+    // soundChomp.play();
   }
-  if (player.x >= (canvas.width-32)) {
-    player.x = 0;
+  if (packman.x >= (canvas.width-32)) {
+    packman.x = 0;
   }
-  if (player.y >= (canvas.height-32)) {
-    player.y = 0;
+  if (packman.y >= (canvas.height-32)) {
+    packman.y = 0;
   }
-  if (player.x < 0) {
-    player.x = (canvas.width - 32);
+  if (packman.x < 0) {
+    packman.x = (canvas.width - 32);
   }
-  if (player.y < 0) {
-    player.y = (canvas.height - 32);
+  if (packman.y < 0) {
+    packman.y = (canvas.height - 32);
   }
   
-  // if (player.pacmouth === 320)  {
-  //   player.pacmouth = 352;
-  // } else {
-  //   player.pacmouth = 320;
-  // }
-  render();
+  if (packman.pacmouth === 320)  {
+    packman.pacmouth = 352;
+  } else {
+    packman.pacmouth = 320;
+  }
+  // render();
 };
 
 function checkReady() {
   this.ready = true;
-  playGame();
+  render();
+  soundBeginning.play();
 }
+
 
 const playGame = () => {
   render();
-  requestAnimationFrame(playGame);
+  if (!isPaused) {
+    requestAnimationFrame(playGame);
+  } else {
+    cancelAnimationFrame(playGame);
+  }
 };
+
+
 
 const myNum = (n) => {
   return Math.floor(Math.random()*n);
@@ -97,6 +143,12 @@ const render = () => {
   context.fillStyle = "black";
   context.fillRect(0,0,canvas.width, canvas.height);
   
+  if (!powerball.visible && powerball.pcountdown < 5) {
+    powerball.x = myNum(420) + 30;
+    powerball.y = myNum(250) + 30;
+    powerball.visible = true;
+  }
+  
   if (!ghost) {
     enemy.ghostNum = myNum(5) * 64;
     enemy.x = myNum(450);
@@ -104,36 +156,124 @@ const render = () => {
     ghost = true;
   }
   
-  if(enemy.moving < 0) {
-    enemy.moving = (myNum (30) * 3) + 10 + myNum(1);
-    enemy.speed = myNum (4);
+  if(enemy.movingTime < 0) {
+    enemy.movingTime = (myNum (20) * 3) + myNum(1);
+    enemy.speed = myNum (3) + 1 ;
     enemy.dirX = 0;
     enemy.dirY = 0;
-    if (enemy.moving % 2) {
-      if (player.x < enemy.x) {
+    
+    if (powerball.ghostEat) {
+      enemy.speed = enemy.speed * -1;
+    }
+    
+    if (enemy.movingTime % 2) {
+      if (packman.x < enemy.x) {
         enemy.dirX = -enemy.speed;
       } else {
         enemy.dirX = enemy.speed;
       }
     } else {
-      if (player.y < enemy.y) {
+      if (packman.y < enemy.y) {
         enemy.dirY = -enemy.speed;
       } else {
         enemy.dirY = enemy.speed;
       }
     }
   }
-  enemy.moving--;
+  enemy.movingTime--;
   enemy.x = enemy.x + enemy.dirX;
   enemy.y = enemy.y + enemy.dirY;
   
+  if (enemy.x >= (canvas.width-32)) {
+    enemy.x = 0;
+  }
+  if (enemy.y >= (canvas.height-32)) {
+    enemy.y = 0;
+  }
+  if (enemy.x < 0) {
+    enemy.x = (canvas.width - 32);
+  }
+  if (enemy.y < 0) {
+    enemy.y = (canvas.height - 32);
+  }
+  
+  // Collision detection ghost
+  if (packman.x <= (enemy.x + 26) && 
+      enemy.x <= (packman.x + 26) && 
+      packman.y <= (enemy.y + 26) && 
+      enemy.y <= (packman.y + 32)
+    ) {
+        console.log('hit a ghost');
+        if (powerball.ghostEat) {
+          score++;
+          soundEatGhost.play();
+        } else {
+          gscore++;
+          soundDeath.play();
+        }
+        packman.x = 10;
+        packman.x = 100;
+        enemy.x = 300;
+        enemy.y = 200;
+        // powerball.ghostEat = false;
+        powerball.pcountdown = 0;
+      }
+      
+  // Collision detection powerball
+  if (packman.x <= (powerball.x) && 
+      powerball.x <= (packman.x + 32) && 
+      packman.y <= (powerball.y) && 
+      powerball.y <= (packman.y + 32  )
+    ) {
+        soundEatFruit.play();
+        console.log('hit');
+        score++;
+        powerball.visible = false;
+        powerball.pcountdown = 500;
+        powerball.ghostNum = enemy.ghostNum;
+        enemy.ghostNum = 384;
+        powerball.x = 0;
+        powerball.y = 0;
+        powerball.ghostEat = true;
+        packman.speed = 18;
+      }
+  if (powerball.ghostEat) {
+    powerball.pcountdown--;
+    if (powerball.pcountdown <= 0) {
+      powerball.ghostEat = false;
+      enemy.ghostNum = powerball.ghostNum;
+      packman.speed = 5;
+    }
+  }    
+
+  if (powerball.visible) {
+    context.fillStyle = "#fff";
+    context.beginPath();
+    context.arc(powerball.x,powerball.y, 10, 0, Math.PI * 2, true);
+    context.closePath();
+    context.fill();
+  }
+  
+  // if (countblink>0){
+  //   countblink--;
+  // } else {
+  //   countblink = 20;
+  // }
+  
+  if (enemy.flash === 0){
+    enemy.flash = 32;
+  } else {
+    enemy.flash = 0;
+  }
+  
   context.font = '20px Verdana';
+  // context.font = "20px 'Press Start 2P', cursive";
   context.fillStyle = '#fff';
 
     context.drawImage(
       mainImage,
       enemy.ghostNum,
-      0,
+      enemy.flash,
       32,
       32,
       enemy.x,
@@ -144,15 +284,23 @@ const render = () => {
   
   context.drawImage(
     mainImage,
-    player.pacmouth,
-    player.pacdir,
+    packman.pacmouth,
+    packman.pacdir,
     32,
     32,
-    player.x,
-    player.y,
+    packman.x,
+    packman.y,
     32,
     32
   );
-
+context.fillStyle = '#FFFF00';
   context.fillText('Pacman: ' + score + ' Ghost: ' + gscore, 2,18 );
+  if (isPaused && !isStart) {
+    context.fillText('PAUSED', 400,20 );
+  }
+  if (isStart) {
+    context.fillText('PLEASE PRESS SPACE TO START', 130,200 );
+    isStart = !isStart;
+  }
+
 };
